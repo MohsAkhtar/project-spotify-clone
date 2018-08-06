59fad4b349f7b859838b90cc0c29c5c855fd819c
MohsAkhtar <mohsin.akhtar0@gmail.com>
2018-08-06 14:12:44 +0100

    Created register form for register page

--- register.php
+++ register.php
@@ -16,9 +16,43 @@
             </p>
             <p>
                 <label for="loginPassword">Password</label>
-            <input type="text" id="loginPassword" name="loginPassword" required>
+            <input type="text" id="loginPassword" name="loginPassword" placeholder="Your password" required>
             </p>
             <button type="submit" name="loginButton">LOG IN</button>
+        </form>
+
+
+        <form id="registerForm" action="register.php" method="POST">
+            <h2>Sign up for an account</h2>
+            <p>
+                <label for="registerUsername">Username</label>
+                <input type="text" id="registerUsername" name="registerUsername" placeholder="e.g. Mohs Akhtar" required>
+            </p>
+            <p>
+                <label for="firstName">First Name</label>
+                <input type="text" id="firstName" name="firstName" placeholder="e.g. Mohs" required>
+            </p>
+            <p>
+                <label for="lastName">Last Name</label>
+                <input type="text" id="lastName" name="lastName" placeholder="e.g. Akhtar" required>
+            </p>
+            <p>
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="e.g. mohs@mail.com" required>
+            </p>
+            <p>
+                <label for="emailConfirm">Email Confirm</label>
+                <input type="email" id="emailConfirm" name="emailConfirm" placeholder="e.g. mohs@mail.com" required>
+            </p>
+            <p>
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Your password" required>
+            </p>
+            <p>
+                <label for="password2">Password Confirm</label>
+                <input type="password" id="password2" name="password2" placeholder="Your password" required>
+            </p>
+            <button type="submit" name="registerButton">SIGN UP</button>
         </form>
     </div>
 </body>
