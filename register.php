f255fc2a5e9f6c88e0245ee563ba234a5c59eca5
40183396 <40183396@live.napier.ac.uk>
2018-09-21 19:20:17 +0100

    Added jQuery to hide login and register fields depending on what span is clicked on register page

--- register.php
+++ register.php
@@ -26,8 +26,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Welcome to SpotifyClone!</title>
+    
     <link rel="stylesheet" href="assets/css/register.css">
+    
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="assets/js/register.js"></script>
 </head>
 <body>
     <div id="background">
