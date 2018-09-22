b79cbe94cc1d63d9dc6b5ac7247ca49e95e97c24
40183396 <40183396@live.napier.ac.uk>
2018-09-22 11:12:49 +0100

    Added icon files for now playing bar

--- index.php
+++ index.php
@@ -19,10 +19,23 @@
     <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 
 <body>
-    <h1>Hi</h1>
+    <div id="nowPlayingBarContainer">
+        <div id="nowPlayingBar">
+            <div id="nowPlayingLeft">
+                
+            </div>
+            <div id="nowPlayingCentre">
+
+            </div>
+            <div id="nowPlayingRight">
+                
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
